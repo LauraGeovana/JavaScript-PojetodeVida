6d07a3b1aa43b0aaecab58081a9e55ff89a06c6d
Laura Geovana <132917999+LauraGeovana@users.noreply.github.com>
2025-06-07 18:31:43 -0300

    .

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0){
         return [dias, horas, minutos, segundos];
     } else {
-        return "0000"
+        return (0,0,0,0)
     }
 }
 
